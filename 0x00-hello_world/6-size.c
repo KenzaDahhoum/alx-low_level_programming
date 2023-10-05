fc5c259d54444833546bf6bffed3a507e6c15f2c
KenzaDahhoum <kenza.dahhoum@gmail.com>
2023-10-05 12:55:13 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
   *Return: always 0(success)
   */
 int main(void)
-{	printf("Size of chart: %zu byte(s)\n", sizeof(char));
+{	printf("Size of char: %zu byte(s)\n", sizeof(char));
 	printf("Size of int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
