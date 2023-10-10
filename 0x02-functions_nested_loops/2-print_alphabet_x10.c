89e22cb94268e0661a0ba42adfe396debcf3346a
KenzaDahhoum <kenza.dahhoum@gmail.com>
2023-10-10 11:17:08 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,13 +10,12 @@ void print_alphabet_x10(void)
 	int i;
 	int j;
 
-	for (j = 0; j <= 10; j++)
+	for (j = 1; j <= 10; j++)
 	{
 		for (i = 97; i <= 122; i++)
 		{
 			_putchar(i);
 		}
 		_putchar('\n');
-		
 	}
 }
