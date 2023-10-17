72667dd6b75c43bfbc51215a9f9fdd71126de31c
KenzaDahhoum <kenza.dahhoum@gmail.com>
2023-10-17 07:25:55 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,12 +7,16 @@
  */
 char *_strcpy(char *dest, char *src)
 {
+	int len = 0;
 	int i;
 
-	while (src[i])
+	while (*(src + len) != '\0')
+		len++;
+
+	for (i = 0; i < len; i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
+	dest[len] = '\0';
 	return (dest);
 }
