011387bdb2d561d3bbd2f11535d64bf75e5b33b5
KenzaDahhoum <kenza.dahhoum@gmail.com>
2023-10-17 04:20:46 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,13 +6,20 @@
   */
 void puts_half(char *str)
 {
-	int i;
+	int i, nb, ln;
+
+	ln = 0;
 
 	for (i = 0; str[i] != '\0'; i++)
-	i++;
-	for (i /= 2; str[i] != '0'; i++)
-	{
+		ln++;
+
+	nb = (ln / 2);
+
+	if ((ln % 2) == 1)
+		nb = ((ln + 1) / 2);
+
+	for (i = nb; str[i] != '\0'; i++)
 		_putchar(str[i]);
-	}
+
 	_putchar('\n');
 }
