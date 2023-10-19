8bfe6e2853610d77e160584b274219963ce42a26
KenzaDahhoum <kenza.dahhoum@gmail.com>
2023-10-19 09:45:55 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,11 +10,11 @@ char *_strcat(char *dest, char *src)
 	int c1, c2;
 
 	c1 = 0;
-	while (dest[c1])
+	while (dest[c1] != '\0')
 	{
 		c1++;
 	}
-	for (c2 = 0; src[c2]; c2++)
+	for (c2 = 0; src[c2] != '\0'; c2++)
 	{
 		dest[c1] = src[c2];
 		c1++;
