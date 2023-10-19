15dddfec178ad0c7d36e70de5136e984ed3ffc15
KenzaDahhoum <kenza.dahhoum@gmail.com>
2023-10-19 09:43:13 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  *_strcat - function that concatenates two strings.
+  *@dest: destination string
+  *@src: source string
+  *Return: dest
+  */
+char *_strcat(char *dest, char *src)
+{
+	int c1, c2;
+
+	c1 = 0;
+	while (dest[c1])
+	{
+		c1++;
+	}
+	for (c2 = 0; src[c2]; c2++)
+	{
+		dest[c1] = src[c2];
+		c1++;
+	}
+	return (dest);
+}
