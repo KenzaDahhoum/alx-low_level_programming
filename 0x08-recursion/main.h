e5f3a9cde2bc16f9a3ca06963def938b6d1d4140
KenzaDahhoum <kenza.dahhoum@gmail.com>
2023-10-26 17:39:42 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,9 @@
+char _putchar(char s);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _sqrt_recursion_helper(int i, int n);
+int _sqrt_recursion(int n);
+int recursive_prime_helper(int n, int i);
+int is_prime_number(int n);
