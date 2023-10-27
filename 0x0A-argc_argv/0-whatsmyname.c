fe4e49e34b8227495758b92c3e4a3b794960943b
KenzaDahhoum <kenza.dahhoum@gmail.com>
2023-10-27 16:57:19 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+  *main - print the name of current file
+  *@argv: argument list
+  *@argc: argument count
+  *Return: 0
+  */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
