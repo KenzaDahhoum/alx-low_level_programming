d576c7dcc3a0ca4b7007286ae54d99bef7542f69
KenzaDahhoum <kenza.dahhoum@gmail.com>
2023-10-30 07:20:50 -0700

    1-strdup.c fixed

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
-*_strdup - function that returns a pointer to a newly allocated space in memory
-*which contains a copy of the string given as a parameter.
+*_strdup - duplicate the buffer
 *@str: string
 *Return: pointer, points to the same value of an pointer
 */
