6a657c86eadddf226fd286b5f806aaa12e61a6b1
KenzaDahhoum <kenza.dahhoum@gmail.com>
2023-11-06 05:57:15 -0800

    1-init_dog.c fixed

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 #include "main.h"
 #include "dog.h"
 /**
@@ -11,7 +11,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	id (d !== NULL)
+	if (d != NULL)
 	{
 		d->name = name;
 		d->age = age;
