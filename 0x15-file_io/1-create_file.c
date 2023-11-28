ffd727a927abfe47840ba331782134f333ebbdd8
KenzaDahhoum <kenza.dahhoum@gmail.com>
2023-11-27 17:00:57 -0800

    fixed task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,11 @@ int create_file(const char *filename, char *text_content)
 
 	file = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 
-	if (file == -1 || filename == NULL)
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (file == -1)
 	{
 		return (-1);
 	}
