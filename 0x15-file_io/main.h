e430741dbbecc69b96047466569981d2c9d149ba
KenzaDahhoum <kenza.dahhoum@gmail.com>
2023-11-27 17:14:54 -0800

    3-cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,4 +1,4 @@
-#ifndef MAINN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
@@ -8,9 +8,11 @@
 #include <stddef.h>
 #include <string.h>
 
+void error_exit(int code, const char *message, const char *filename, int fd);
 ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif
+#endif /* MAIN_H */
+
