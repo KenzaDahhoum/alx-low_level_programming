630c0ce1bd9c2485c50f42da97c636b9fb1cd773
KenzaDahhoum <kenza.dahhoum@gmail.com>
2023-11-27 16:34:19 -0800

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+  *create_file - create a file
+  *@filename: file name
+  *@text_content: buffer
+  *Return: 1 on success otherwise -1;
+  */
+int create_file(const char *filename, char *text_content)
+{
+	int size, file, i = 0;
+
+	file = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+
+	if (file == -1 || filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		return (1);
+	}
+	while (*(text_content + 1))
+	{
+		i++;
+	}
+	size = write(file, text_content, i);
+
+	if (size == -1)
+	{
+		return (-1);
+	}
+
+	close(file);
+	return (1);
+
+}
